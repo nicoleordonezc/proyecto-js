52f7471d59b0468f35475f622693992270875d37
Nicoleordonezc <nicoloe24@gmail.com>
2025-04-08 22:50:21 -0500

    feat: :construction: Search bar in progress

--- js/main.js
+++ js/main.js
@@ -2,6 +2,30 @@
     const url = new URL("https://api.api-ninjas.com/v1/exercises");
 
 
+    // fetch(url,{
+    //     method: 'GET',
+    //     headers:{
+    //         'x-api-key': 'bijlCIvPMXFto3M7dXZV3TJUxcJKJldS4Qi8izG8'
+    //     }
+    // })
+    // .then (respuesta => respuesta.json())
+    // .then ( info => {
+    //     info.forEach(element => {
+    //         const ejercicio = element.name
+    //         console.log(ejercicio);
+    //     });
+        
+    // })
+    // .catch(error =>{
+    //     console.error('hubo un error:', error)
+    // });
+    
+  
+    const miForm = document.getElementById("form");
+
+    miForm.onsubmit = (event) => {
+        event.preventDefault();
+        var nombreDelCampo = miForm.busqueda.value;
     fetch(url,{
         method: 'GET',
         headers:{
@@ -10,10 +34,15 @@
     })
     .then (respuesta => respuesta.json())
     .then ( info => {
-        console.log('datos:', info)
+        // info.forEach(element => {
+        //     const ejercicio = element.name
+        //     console.log(ejercicio);
+        // });
+        const filtro = info.filter(ejercicios => ejercicios.name=== 'string' && ejercicios.name.toLowercase().includes(nombreDelCampo))
+        console.log(filtro);
+        
     })
     .catch(error =>{
         console.error('hubo un error:', error)
-    });
-    
-  
+    })
+    }
